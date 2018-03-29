817cad05a360f2cfdc7153db03dcb1727e294dad
Philip Rosen <philipjrosen@gmail.com>
2018-03-28 22:24:27 -0700

    create save course thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,11 +5,33 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
 export function loadCourses() {
   return dispatch => {
     return CourseApi.getAllCourses()
       .then(courses => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return CourseApi.saveCourse(course)
+      .then(course => {
+        course.id
+          ? dispatch(updateCourseSuccess(course))
+          : dispatch(createCourseSuccess(course));
       })
       .catch(error => {
         throw error;
